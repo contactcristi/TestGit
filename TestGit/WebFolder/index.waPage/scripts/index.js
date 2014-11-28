945da2e6fd22932cb7dbee2a512c0d485fde9acc
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-11-28 18:59:45 +0200

    Update #16

---
+++ TestGit/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,35 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var documentEvent = {};	// @document
+	var login1 = {};	// @login
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		if (WAF.directory.currentUser() == null) {
+			$$('main').loadComponent ('Public.waComponent');
+		} else {
+			$$('main').loadComponent ('App1.waComponent');
+		}
+	};// @lock
+
+	login1.logout = function login1_logout (event)// @startlock
+	{// @endlock
+		window.location = 'index.html';
+	};// @lock
+
+	login1.login = function login1_login (event)// @startlock
+	{// @endlock
+			window.location = 'index.html';
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+	WAF.addListener("login1", "logout", login1.logout, "WAF");
+	WAF.addListener("login1", "login", login1.login, "WAF");
+// @endregion
+};// @endlock
